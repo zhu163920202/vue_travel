a86c0e2df2988aeb7675e9f9c52f9e8f12246b9a
zhl <zhu163920202@163.com>
2020-03-19 14:54:46 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 将faskclick方法绑定到document.body上
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<!-- 对外只能暴露一个根标签 -->
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+</style>
