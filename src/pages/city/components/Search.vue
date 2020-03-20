9fc98042f06161c5d7c97576ec0a871d417771ca
zhl <zhu163920202@163.com>
2020-03-20 17:01:21 +0800

    citysearch

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名或拼音" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background-color: $bgColor
+    .search-input
+      width: 100%
+      height: .62rem
+      line-height: .62rem
+      box-sizing border-box
+      padding: 0 .1rem
+      text-align: center
+      border-radius: .06rem
+</style>
